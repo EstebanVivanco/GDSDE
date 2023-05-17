10c5b18a457a8baf8637c9d19bb541c030b1fb6a
TasmVanum <gaston.cath14@gmail.com>
2023-05-17 01:54:53 -0400

    correcciones en tablas generales

--- public/navbarC.js
+++ public/navbarC.js
@@ -1,3 +1,14 @@
+        /* Set the width of the side navigation to 250px */
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+                
+        }
+
+        /* Set the width of the side navigation to 0 */
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        } 
+
 class navbinBombin extends HTMLElement{
     constructor(){
         super();
